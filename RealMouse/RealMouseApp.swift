bb35d00dc4cfd1e52688c5428a13791cf76f3aae
Josh Bedwell <rcxwhiz@gmail.com>
2022-04-23 11:21:30 -0600

    Initial Commit

---
+++ RealMouse/RealMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealMouseApp.swift
+//  RealMouse
+//
+//  Created by Josh Bedwell on 4/23/22.
+//
+
+import SwiftUI
+
+@main
+struct RealMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
